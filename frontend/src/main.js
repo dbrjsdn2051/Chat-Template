33da1750fd69483566b83bcbbe6c4aceeb82db02
dbrjsdn2051 <dbrjdn2051@naver.com>
2025-10-07 22:30:42 +0900

    feat: frontend page feature

---
+++ frontend/src/main.js
@@ -0,0 +1,20 @@
+import {createApp} from 'vue'
+import App from './App.vue'
+import vuetify from "@/plugins/vuetify.js";
+import router from "@/router/index.js";
+import axios from "axios";
+
+const app = createApp(App);
+
+axios.interceptors.request.use(config => {
+        config.headers.Authorization = `Bearer ${localStorage.getItem('token')}`
+        return config;
+    },
+    error => {
+        Promise.reject(error);
+    })
+
+app.use(router)
+app.use(vuetify)
+app.use(axios)
+app.mount("#app")
